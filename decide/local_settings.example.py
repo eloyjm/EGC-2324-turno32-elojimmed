948cad8c60af835dfbdbc94841f669c0790d9479
eloyjm <eloyjm93@gmail.com>
2023-11-30 16:47:36 +0100

    fix:local_setting example

--- decide/local_settings.example.py
+++ decide/local_settings.example.py
@@ -29,7 +29,7 @@ BASEURL = 'http://10.5.0.1:8000'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql,
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'HOST': 'db',
